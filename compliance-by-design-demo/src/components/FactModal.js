e7c6d386235db012c6b1ba91d04022de9d1c9403
Pim Otte <potte@quintor.nl>
2019-11-27 11:10:28 +0100

    Temp fixes for lists

--- compliance-by-design-demo/src/components/FactModal.js
+++ compliance-by-design-demo/src/components/FactModal.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './FactModal.css'
 class FactModal extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+            'factValue': null
+        }
+    }
     handleAffirm() {
-        this.props.handleResult(true)
+        const result = this.state.factValue || true
+        this.props.handleResult(result)
     }
 
     handleDeny() {
         this.props.handleResult(false)
+    }
+
+    handleInput(event) {
+        console.log(event.target.value)
+        this.setState({
+            'factValue': event.target.value
+        })
     }
 
     render() {
@@ -16,8 +30,9 @@ class FactModal extends Component{
             <div className={showHideClassName}>
                 <section className="modal-main">
                     <p>Is {this.props.fact} van toepassing?</p>
+                    <input className="value display-block" placeholder="Waarde van feit" onChange={this.handleInput.bind(this)}/>
                     <button className="affirm" onClick={this.handleAffirm.bind(this)}>Yes</button>
-                    <button className="deny" onClick={this.handleDeny.bind(this)}>No</button>
+                    <button className="deny" disabled={this.state.factValue} onClick={this.handleDeny.bind(this)}>No</button>
                 </section>
             </div>
         );
