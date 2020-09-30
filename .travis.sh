45c88e4c7198270722b265c13f64d4eebf6c0a15
Daron Brison <dbrison@quintor.nl>
2020-09-30 11:44:04 +0200

    Updated dependencies

--- .travis.sh
+++ .travis.sh
@@ -4,7 +4,7 @@
 set -e
 
 cd compliance-by-design-demo
-[ $(date +%s) -lt 1630447200 ] || npm audit  # Temporarily disable audit manual review required
+npm audit
 npm install
 npm start &
 cypress run
