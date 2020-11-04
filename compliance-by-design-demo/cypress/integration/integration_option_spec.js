664f73c974cbc37bf0762ef7dc23ef764f22d75a
blackstardlb <brisondl@gmail.com>
2020-11-04 12:13:31 +0100

    Fixed test

--- compliance-by-design-demo/cypress/integration/integration_option_spec.js
+++ compliance-by-design-demo/cypress/integration/integration_option_spec.js
@@ -17,7 +17,7 @@ describe('The demo', function() {
 
         performAct('<<besluiten de aanvraag niet te behandelen>>')
 
-        chooseOption('[aanvraag]', 1)
+        chooseOption('[aanvraag]', "Act 2 from Leraar 1")
         answerQuestion('[aanvrager heeft voldaan aan enig wettelijk voorschrift voor het in behandeling nemen van de aanvraag]', 'No')
         answerQuestion('[aanvrager heeft de gelegenheid gehad de aanvraag aan te vullen]', 'Yes')
         answerQuestion('[de aanvraag is binnen de afgelopen 4 weken aangevuld]', 'No')
